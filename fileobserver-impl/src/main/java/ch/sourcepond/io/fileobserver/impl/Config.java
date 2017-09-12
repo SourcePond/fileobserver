918be78d55b38c0319a8ae53fa89b6344cdeef0c
Roland Hauser <sourcepond@gmail.com>
2017-09-12 13:01:19 +0200

    Increased default delay timeout

--- fileobserver-impl/src/main/java/ch/sourcepond/io/fileobserver/impl/Config.java
+++ fileobserver-impl/src/main/java/ch/sourcepond/io/fileobserver/impl/Config.java
@@ -35,5 +35,5 @@ public @interface Config {
             description = "Duration to wait until a received event is actually dispatched. Necessary" +
                     " to avoid duplicate events."
     )
-    long eventDispatchDelayMillis() default 1000L;
+    long eventDispatchDelayMillis() default 3000L;
 }
